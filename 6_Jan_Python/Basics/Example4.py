bc1347eb7f0c30cb6bbdca699c513f2fe7a7afde
dharmishthakothari <dharmishtha.only@gmail.com>
2026-01-13 09:04:42 +0530

    Add files via upload

---
+++ 6_Jan_Python/Basics/Example4.py
@@ -0,0 +1,6 @@
+no1=int(input("Please enter no1 "))
+no2=int(input("Please enter no2 "))
+sum=no1+no2
+print(sum)
+print("sum of ",no1,"and",no2,"is",sum)
+print(f"Sum of {no1} and {no2} is {sum}")
---
+++ 6_Jan_Python/Basics/Example5.py
@@ -0,0 +1,10 @@
+no1=int(input("Enter number 1 "))
+no2=int(input("Enter number 2 "))
+sum=no1+no2
+sub=no1-no2
+mul=no1*no2
+div=no1/no2
+print(f"Addition of {no1} and {no2} is {sum}")
+print(f"Substraction of {no1} and {no2} is {sub}")
+print(f"Multilication of {no1} and {no2} is {mul}")
+print(f"Division of {no1} and {no2} is {div}")
