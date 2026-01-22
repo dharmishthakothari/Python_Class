637fa141c24bc36943520c5343f7ceb5d5df2b7e
dharmishthakothari <dharmishtha.only@gmail.com>
2026-01-22 08:55:42 +0530

    Add files via upload

---
+++ 6_Jan_Python/Basics/Example12.py
@@ -0,0 +1,12 @@
+#string
+name=input("Enter your name ")
+print("a" in name)
+lst=['mahek','netra','zankhna']
+# list of names
+print("mahek" in lst)
+print('dharmishtha' in lst)
+
+#list of numeric
+lst1=[1,2,4,7,8]
+print(2 in lst1)
+print(22 in lst1)
