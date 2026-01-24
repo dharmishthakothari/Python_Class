f1b8950a6830699a9aa951dc92c26b6da795cdef
dharmishthakothari <dharmishtha.only@gmail.com>
2026-01-24 08:56:45 +0530

    Add files via upload

---
+++ 6_Jan_Python/Conditional_Stmt_Loops/Example2.py
@@ -0,0 +1,5 @@
+age=int(input("Enter age "))
+if age>18:
+    print("User is adult")
+else:
+    print("User is not adult")
---
+++ 6_Jan_Python/Conditional_Stmt_Loops/Example4.py
@@ -0,0 +1,5 @@
+number=int(input("Enter number "))
+if number%5==0 and number%7==0:
+    print("number is divisable by 5 & 7")
+else:
+    print("number is not divisable by 5 and 7")
