434adc2001d1525d7315bbe66855b7eaf6b49c15
dharmishthakothari <dharmishtha.only@gmail.com>
2026-02-03 08:57:30 +0530

    Add files via upload

---
+++ 6_Jan_Python/Conditional_Stmt_Loops/Loop_Example2.py
@@ -0,0 +1,12 @@
+# sum of 1 to 10 numbers 
+sum=0
+for i in range(1,11):
+    #sum=sum+i
+    sum+=i
+print(f"Summation of 1 - 10 numbers {sum}")
+
+# multiple 1 to 10 numbers
+mul=1
+for i in range(1,11):
+    mul*=i
+print(f"Multiplication 1-10 numbers is {mul}")
---
+++ 6_Jan_Python/Conditional_Stmt_Loops/Loop_Example3.py
@@ -0,0 +1,9 @@
+#Factorial of given number
+num=int(input("Enter number "))
+ans=1
+#for i in range(num,0,-1):
+for i in range(1,num+1):
+    ans=ans*i
+    
+
+print(f"Factorial of {num} is {ans}")
