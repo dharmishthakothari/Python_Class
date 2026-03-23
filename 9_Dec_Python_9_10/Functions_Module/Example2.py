8969ba44654138955477afa3824f74cb5e650120
dharmishthakothari <dharmishtha.only@gmail.com>
2026-03-23 10:04:08 +0530

    Delete 9_Dec_Python_9_10/Functions_Module directory

--- 9_Dec_Python_9_10/Functions_Module/Example2.py
+++
@@ -1,9 +0,0 @@
-# create function greet , to greet user (paramter)
-def greet(name):
-    print("Good monring ",name)
-
-#calling function
-i=input("Enter your name ")
-greet(i)
-i=input("Enter your name ")
-greet(i)
